d4ffd51451a340ae713926b9e99d88a92e28ff7d
Beerendra M C <beerendramc@gmail.com>
2020-09-28 17:01:57 +0530

    first commit

---
+++ precache-manifest.2eb7cb7d790198747a05f0fa43c9fe5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1179cdfe7bc6972bbfc5336a34a49a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d9f40afa94ceda018403",
+    "url": "/portfolio/static/css/main.4e60335b.chunk.css"
+  },
+  {
+    "revision": "52d41ccb6d6ac5eabfb3",
+    "url": "/portfolio/static/js/2.64676749.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.64676749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f40afa94ceda018403",
+    "url": "/portfolio/static/js/main.05de0234.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
