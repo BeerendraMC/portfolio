2966863402f4fc83bf8b25a8dbe7ce56334ebc07
Beerendra M C <beerendramc@gmail.com>
2021-05-09 16:10:36 +0530

    changed to BrowserRouter

---
+++ precache-manifest.ec4554fe7db3d5c7aa5ede742917ce79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9b0df2e3639ea6addc58491a96926b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edf4a77806bb5cdc05d6",
+    "url": "/static/css/main.bc4649de.chunk.css"
+  },
+  {
+    "revision": "e21c013300e2a715ea6c",
+    "url": "/static/js/2.8f6a1cab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8f6a1cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf4a77806bb5cdc05d6",
+    "url": "/static/js/main.0cfcb6d9.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
